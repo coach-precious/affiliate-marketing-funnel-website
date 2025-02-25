3c994f72ff8281b110637235409e69a1cf6e9237
unknown <holuwasayofunmiprecious@gmail.com>
2025-02-26 00:53:00 +0100

    feat: update to master class

--- app/components/master-class/coach.tsx
+++ app/components/master-class/coach.tsx
@@ -3,6 +3,7 @@ import coachPrecious1 from "~/assets/images/coach_precious.jpg";
 import coachPrecious3 from "~/assets/images/coach_precious_3.jpg";
 import coachPrecious4 from "~/assets/images/coach_precious_4.jpg";
 import coachPrecious5 from "~/assets/images/coach_precious_5.jpg";
+import MasterClassJoinBtn from "./join-btn";
 
 const images = [coachPrecious3, coachPrecious4, coachPrecious5];
 
@@ -17,7 +18,7 @@ export default function MasterClassCoach() {
       <h2 className="text-2xl md:text-3xl font-bold text-center mb-4 text-white">
         But before I continue, let me introduce myself...
       </h2>
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-10">
         <motion.img
           src={coachPrecious1}
           alt="Coach Precious"
@@ -27,28 +28,30 @@ export default function MasterClassCoach() {
           viewport={{ once: true, amount: 0.3 }}
         />
 
-        <p className="text-lg mb-8 text-gray-300">
-          My name is <span className="font-black">Ogunleye Precious</span>
-          <span className="text-pink-400">(Coach Precious)</span>, a Top rated
-          Digital Affiliate Marketer that has been able to win car, traveled to
-          different countries on vacation and In just 24 months, she has
-          generated over $36,000 in sales from her digital product business,
-          quickly establishing herself as a respected authority in the field..
-        </p>
+        <div className="flex flex-col gap-5">
+          <p className="text-lg text-gray-300">
+            My name is <span className="font-black">Ogunleye Precious</span>
+            <span className="text-pink-400">(Coach Precious)</span>, a Top rated
+            Digital Affiliate Marketer that has been able to win car, traveled
+            to different countries on vacation and In just 24 months, she has
+            generated over $36,000 in sales from her digital product business,
+            quickly establishing herself as a respected authority in the field..
+          </p>
 
-        <p className="text-lg mb-8 text-gray-300">
-          I was once a broke girl who was feeding from hand to mouth and could
-          barely afford basic needs for myself.
-        </p>
+          <p className="text-lg text-gray-300">
+            I was once a broke girl who was feeding from hand to mouth and could
+            barely afford basic needs for myself.
+          </p>
 
-        <p className="text-lg mb-8 text-gray-300">
-          Until I came across this life changing opportunity online, and in a
-          few weeks my life totally transformed. I am now a{" "}
-          <span className="text-pink-400">proud millionaire</span>
-          who helps other serious minded people change their financial life.
-        </p>
+          <p className="text-lg text-gray-300">
+            Until I came across this life changing opportunity online, and in a
+            few weeks my life totally transformed. I am now a{" "}
+            <span className="text-pink-400">proud millionaire</span>
+            who helps other serious minded people change their financial life.
+          </p>
+        </div>
 
-        <div className="grid gap-5 grid-cols-3 md:grid-cols-3">
+        <div className="grid gap-5 grid-cols-3">
           {images.map((image, index) => {
             return (
               <motion.img
@@ -63,6 +66,8 @@ export default function MasterClassCoach() {
             );
           })}
         </div>
+
+        <MasterClassJoinBtn />
       </div>
     </motion.section>
   );
--- app/components/master-class/hero.tsx
+++ app/components/master-class/hero.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
-import { FaHandPointUp } from "react-icons/fa";
 import { IoStar, IoStarHalf } from "react-icons/io5";
+import MasterClassJoinBtn from "./join-btn";
 
 import bg from "~/assets/images/master-class/dollar.webp";
 
@@ -61,17 +61,7 @@ export default function MasterClassHero() {
             status.
           </motion.p>
 
-          <motion.a
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ delay: 0.2 }}
-            className="bg-gradient-to-br from-yellow-400 to-orange-500 px-4 py-3 text-xl md:text-2xl lg:text-3xl md:py-4 lg:py-5 rounded-lg flex text-center items-center justify-center gap-2 max-w-max mx-auto  text-white hover:from-orange-800 hover:to-yellow-700 font-bold"
-            href=""
-          >
-            <span className="">Yes, I want the live video training</span>
-            <FaHandPointUp />
-          </motion.a>
+          <MasterClassJoinBtn />
 
           <motion.div
             initial={{ opacity: 0, y: 20 }}
