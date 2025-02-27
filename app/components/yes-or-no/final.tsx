0d63aa8b68cdf309da497959910d741fe95f1775
unknown <holuwasayofunmiprecious@gmail.com>
2025-02-27 23:06:17 +0100

    feat: add final page for yes or no

---
+++ app/components/yes-or-no/final.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Final = () => {
+  return (
+    <div className="flex flex-col gap-8 italic">
+      <h3 className="italic">
+        <span className="font-bold text-xl underline text-yellow-300">
+          Okay
+        </span>
+        . I am out of questions.
+      </h3>
+      <h3>
+        From your responses, It looks like you are a good fit for this income
+        opportunity.
+      </h3>
+      <h3>Most people don't make it this far.</h3>
+      <h3>
+        Right now, you are going to get access to the FULL details of the
+        legitimate money making opportunity that can change your life.
+      </h3>
+
+      <h3>Don't rush.</h3>
+    </div>
+  );
+};
+
+export default Final;
