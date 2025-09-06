c64a2acb2e19802605e47bf0ace75a55e14c48c8
unknown <holuwasayofunmiprecious@gmail.com>
2025-09-06 14:06:46 +0100

    feat: update main page link

--- app/links.ts
+++ app/links.ts
@@ -1,6 +1,6 @@
 export const links = {
   masterClass: "https://taplink.cc/coach_precious",
   yesOrNo: "https://taplink.cc/coach_precious",
-  main: "https://taplink.cc/coach_precious",
+  main: "https://taplink.cc/coach_preciouss",
 };
 // main: "https://wa.link/o6aogw",
