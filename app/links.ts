95862630f3adeeae7fced242d21b91c1843a672c
unknown <holuwasayofunmiprecious@gmail.com>
2025-09-06 13:51:19 +0100

    feat: update the links

--- app/links.ts
+++ app/links.ts
@@ -1,6 +1,6 @@
 export const links = {
-  masterClass: "https://chat.whatsapp.com/Dss1Qsqj4ia41zVMxjTC8z",
-  yesOrNo: "https://chat.whatsapp.com/Dss1Qsqj4ia41zVMxjTC8z",
-  main: "https://chat.whatsapp.com/Dss1Qsqj4ia41zVMxjTC8z",
+  masterClass: "https://taplink.cc/coach_precious",
+  yesOrNo: "https://taplink.cc/coach_precious",
+  main: "https://taplink.cc/coach_precious",
 };
 // main: "https://wa.link/o6aogw",
