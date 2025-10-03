98a15e0537524a398ac2d5c8f3c03e426703cf8e
unknown <holuwasayofunmiprecious@gmail.com>
2025-10-03 16:34:49 +0100

    fix: link

--- app/links.ts
+++ app/links.ts
@@ -1,5 +1,6 @@
 export const links = {
-  masterClass: "https://taplink.cc/coach_precious",
+  masterClass:
+    "https://chat.whatsapp.com/JoCyYX5aOETE0GcyGm8gne?mode=ems_copy_t",
   yesOrNo: "https://taplink.cc/coach_preciouss",
   main: "https://chat.whatsapp.com/Gy0m399WDWh9qHuz5UF3en?mode=ems_copy_t",
 };
