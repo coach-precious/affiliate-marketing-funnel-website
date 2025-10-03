b5787ca81809396053ac49c012e336672798937c
unknown <holuwasayofunmiprecious@gmail.com>
2025-10-03 15:47:19 +0100

    fix: links

--- app/links.ts
+++ app/links.ts
@@ -1,7 +1,6 @@
 export const links = {
-  masterClass:
-    "https://chat.whatsapp.com/JoCyYX5aOETE0GcyGm8gne?mode=ems_copy_t",
-  yesOrNo: "https://chat.whatsapp.com/DNrjJFW5T2wJT2Atk1Vc9H?mode=ems_copy_t",
+  masterClass: "https://taplink.cc/coach_precious",
+  yesOrNo: "https://taplink.cc/coach_preciouss",
   main: "https://chat.whatsapp.com/Gy0m399WDWh9qHuz5UF3en?mode=ems_copy_t",
 };
 // main: "https://wa.link/o6aogw",
