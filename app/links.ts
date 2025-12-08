fdb4d813df8009b9d53733ed2e9161baf00b3062
unknown <holuwasayofunmiprecious@gmail.com>
2025-12-08 10:35:19 +0100

    feat: add tiktok link

--- app/links.ts
+++ app/links.ts
@@ -2,6 +2,6 @@ export const links = {
   masterClass: "https://chat.whatsapp.com/D1xP5jLMwM57BZWE1iNNjQ?mode=wwt",
   yesOrNo: "https://chat.whatsapp.com/HOOueiCGyzsJuwTRBhsar4?mode=wwt",
   main: "https://chat.whatsapp.com/JoCyYX5aOETE0GcyGm8gne?mode=wwt",
-  tiktok: "",
+  tiktok: "https://t.me/+oGlmrm2qJ-5kNjZk",
 };
 // main: "https://wa.link/o6aogw",
