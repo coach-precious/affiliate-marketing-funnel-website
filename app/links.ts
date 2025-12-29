0181a831e36ca3b9362b8e5b4429b3263b38c3d1
unknown <holuwasayofunmiprecious@gmail.com>
2025-12-29 19:23:04 +0100

    update

--- app/links.ts
+++ app/links.ts
@@ -1,5 +1,5 @@
 export const links = {
-  masterClass: "https://t.me/+GDkeK8UQFZZkOWQ0w",
+  masterClass: "https://t.me/+GDkeK8UQFZZkOWQ0",
   yesOrNo: "https://chat.whatsapp.com/HOOueiCGyzsJuwTRBhsar4?mode=wwt",
   main: "https://chat.whatsapp.com/JoCyYX5aOETE0GcyGm8gne?mode=wwt",
   tiktok: "https://t.me/+oGlmrm2qJ-5kNjZk",
